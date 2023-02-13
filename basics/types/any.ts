111a2c216af224a607c98f722a8b288a94dd0b5a
Carlos Gámez <hardpx@gmail.com>
2023-02-13 11:57:59 -0600

    any

---
+++ basics/types/any.ts
@@ -0,0 +1,14 @@
+(() => {
+  let game: any = 0;
+  let inStore;
+  let rank;
+
+  game = 'Hades';
+  console.log(game.charAt(0));
+
+  game = 123.523455;
+  console.log(game.toFixed(2));
+
+  console.log(inStore);
+  console.log(rank);
+})();
