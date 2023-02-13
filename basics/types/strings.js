d8dc43b7aacc58ea08b48e529da2cdee4d2b8951
Carlos Gámez <hardpx@gmail.com>
2023-02-13 11:46:40 -0600

    strings

---
+++ basics/types/strings.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+    const name = 'Ernest';
+    const lasName = 'Adame';
+    const nickname = `Neto`;
+    console.log(`Hello, my name is ${name}`);
+})();
---
+++ basics/types/strings.ts
@@ -0,0 +1,7 @@
+(() => {
+  const name: string = 'Ernest';
+  const lasName: string = 'Adame';
+  const nickname: string = `Neto`;
+
+  console.log(`Hello, my name is ${name}`);
+})();
